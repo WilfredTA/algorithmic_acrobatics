d40a3b18080836ba8581a4a064c948b9426045bf
Tannr Allard <rockinghamtannr@gmail.com>
2018-01-20 09:08:19 -0500

    Adding debug info to LIS

--- Recursion/longest_increasing_subsequence.rb
+++ Recursion/longest_increasing_subsequence.rb
@@ -13,6 +13,8 @@
 # if nums[n-1] < nums[n], then lis(n) == lis(n-1) + 1
 # Otherwise, lis(n) == lis(n - 1)
 
+
+# With new_start
 def length_of_lis(nums)
   return 0 if nums.length < 1
   lis(nums, nums.length - 1)
@@ -36,3 +38,30 @@ p length_of_lis([4,10,4,3,8,9]) # Should be 3
 
 
 p length_of_lis([1,3,6,7,9,4,10,5,6]) # Returns 5 but should return 6
+
+
+# Without new_start
+
+def length_of_lis(nums)
+  return 0 if nums.length < 1
+  lis(nums, nums.length - 1)
+end
+
+def lis(nums, end_idx)
+  if end_idx < 1
+    return 1
+  end
+  
+  if nums[end_idx] > nums[end_idx - 1]
+    lis(nums, end_idx - 1) + 1
+  else
+    lis(nums, end_idx-1)
+  end  
+end
+
+p length_of_lis([10, 9, 2, 5, 3, 7, 101, 18]) # Should be 4
+p length_of_lis([4,10,4,3,8,9]) # Returns 4 should return 3
+
+
+p length_of_lis([1,3,6,7,9,4,10,5,6]) # Returns 7 but should return 6
+
