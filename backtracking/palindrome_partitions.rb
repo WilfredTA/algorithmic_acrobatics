3030ce4c7a02a950ea1f98e182cf35c6bea86d5c
Tannr Allard <tallard@localhost.localdomain>
2018-01-31 15:19:38 -0500

    Adding palindrome partitions with backtracking

---
+++ backtracking/palindrome_partitions.rb
@@ -0,0 +1,32 @@
+def partition(s)
+    result = []
+    partition_helper(s, result, [], 0)
+    result
+end
+
+
+def partition_helper(string, result, solution, position)
+
+  if position == string.length
+    result << solution.clone unless solution.empty?
+  else
+    i = position
+    while i < string.length do
+      substring = string[position..i]
+      if substring == substring.reverse
+        solution << substring
+        partition_helper(string, result, solution, i + 1)
+        solution.pop
+      end
+
+      i += 1
+    end
+  end
+end
+
+p partition("aab")
+
+# Generates the following substrings in the following order:
+# "a", "a", "b", "ab", "aa", "b", "aab"
+
+# SHOULD return this as the result: [['a', 'a', 'b'], ['aa', 'b']]
